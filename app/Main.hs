0a681a66e6f0cd448847e6cb61eba7517adb1db9
Daniel Barry <dbarry@stevens.edu>
2020-08-17 06:31:26 -0400

    Add unlink feature

--- app/Main.hs
+++ app/Main.hs
@@ -1,68 +1,112 @@
+#!/usr/bin/env runhaskell
+
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
 import           Prelude                 hiding ( lookup )
 import qualified Data.Map                      as Map
 import           Data.Map                       ( Map )
 import           Data.Set                       ( Set )
+import           System.Directory               ( removeDirectoryLink )
+import           System.FilePath                ( dropTrailingPathSeparator )
 import           Data.Digest.Pure.SHA
+import           Path
+import           Path.IO
 import           SmDedupe
 import           SmDedupe.Parse
-import           System.Directory
+import           System.Console.CmdArgs
 import           System.Environment
-import           System.FilePath
 import           Text.Parsec.ByteString
+
+data Opts = Opts {directory :: FilePath, unlink :: Bool}
+              deriving (Show, Data, Typeable)
+
+opts :: Opts
+opts =
+  Opts
+      { directory = def &= args &= typ "DIR"
+      , unlink    = def
+        &= help "Undo symlinks and copy over their target directories"
+      }
+    &= program "sm-dedupe"
+    &= summary "sm-dedupe v0.1.0, (C) Daniel Barry"
+    &= help "A tool to remove duplicates from your Stepmania songs folder"
 
 -- | Go through given paths recursively, find .sm files, get their chartkeys, check for
 -- duplicates, and replace duplicates with a symlink
 dedupe
-  :: (Show t, Num t)
-  => Map (Set (Digest SHA1State)) String
-  -> t
-  -> [FilePath]
-  -> IO String
-dedupe _ symlinks [] =
-  return $ "Replaced " ++ show symlinks ++ " new duplicate(s) with symlinks"
-dedupe songs symlinks (path : paths) = do
-  isFile        <- doesFileExist path
-  canonicalPath <- canonicalizePath path
-  relativePath  <- makeRelativeToCurrentDirectory canonicalPath
-  if path == relativePath
-    then if isExtensionOf "sm" canonicalPath && isFile
-      then do
-        parseResult <- parseFromFile parseSm canonicalPath
-        case parseResult of
-          Left parseError -> return $ show parseError
-          Right steps ->
-            let chartkeys = getChartkeys steps
-                parent    = takeDirectory canonicalPath
-            in  case Map.lookup chartkeys songs of
-                  Just target -> do
-                    putStrLn
-                      $  "Found duplicate of "
-                      ++ target
-                      ++ " in "
-                      ++ parent
-                    putStrLn "Creating symlink..."
-                    removeDirectoryRecursive parent
-                    createDirectoryLink target parent
-                    dedupe songs (symlinks + 1) paths
-                  Nothing ->
-                    dedupe (Map.insert chartkeys parent songs) symlinks paths
-      else do
-        isDirectory <- doesDirectoryExist path
-        if isDirectory
-          then do
-            contents <- getDirectoryContents path
-            dedupe songs symlinks
-              $  map (path </>) (filter notDot contents)
-              ++ paths
-          else dedupe songs symlinks paths
-    else dedupe songs symlinks paths
+  :: (Show a, Num a)
+  => Map (Set (Digest SHA1State)) (Path Abs Dir)
+  -> a
+  -> [Path Abs Dir]
+  -> [Path Abs File]
+  -> IO [Char]
+dedupe songs affected [] [] =
+  return $ "Replaced " ++ show affected ++ " new duplicate(s) with symlinks"
+dedupe songs affected (currentDirectory : directories) [] = do
+  symlink <- isSymlink currentDirectory
+  if symlink
+    then dedupe songs affected directories []
+    else do
+      (newDirectories, newFiles) <- listDir currentDirectory
+      dedupe songs affected (directories ++ newDirectories) newFiles
+dedupe songs affected directories (currentFile : files) = do
+  case fileExtension currentFile of
+    Right ".sm" -> do
+      parseResult <- parseFromFile parseSm $ toFilePath currentFile
+      case parseResult of
+        Left parseError -> return $ show parseError
+        Right steps ->
+          let chartkeys       = getChartkeys steps
+              parentDirectory = parent currentFile
+          in  case Map.lookup chartkeys songs of
+                Just target -> do
+                  putStrLn
+                    $  "Found duplicate of "
+                    ++ toFilePath target
+                    ++ " in "
+                    ++ toFilePath parentDirectory
+                  putStrLn "Creating symlink..."
+                  removeDirRecur parentDirectory
+                  createDirLink target parentDirectory
+                  dedupe songs (affected + 1) directories files
+                Nothing -> dedupe
+                  (Map.insert chartkeys parentDirectory songs)
+                  affected
+                  directories
+                  files
+    _ -> dedupe songs affected directories files
+
+undo :: (Show t, Num t) => t -> [Path Abs Dir] -> IO [Char]
+undo affected [] = return $ "Unlinked " ++ show affected ++ " directories"
+undo affected (currentDirectory : directories) = do
+  symlink <- isSymlink currentDirectory
+  if symlink
+    then do
+      targetPath <- getSymlinkTarget currentDirectory
+      case parseAbsDir targetPath of
+        Right target -> do
+          putStrLn
+            $  "Unlinking "
+            ++ show currentDirectory
+            ++ " -> "
+            ++ show target
+          removeDirectoryLink $ dropTrailingPathSeparator $ toFilePath
+            currentDirectory
+          copyDirRecur target currentDirectory
+          (newDirectories, _) <- listDir currentDirectory
+          undo (affected + 1) $ directories ++ newDirectories
+        _ -> undo affected directories
+    else do
+      (newDirectories, _) <- listDir currentDirectory
+      undo affected $ directories ++ newDirectories
 
 main :: IO ()
 main = do
-  (dir : _) <- getArgs
-  setCurrentDirectory dir
-  paths  <- getDirectoryContents "."
-  result <- dedupe Map.empty 0 (filter notDot paths)
+  parsedOpts  <- cmdArgs opts
+  parsedDir   <- parseRelDir $ directory parsedOpts
+  absoluteDir <- makeAbsolute parsedDir
+  result      <- if unlink parsedOpts
+    then undo 0 [absoluteDir]
+    else dedupe Map.empty 0 [absoluteDir] []
   putStrLn result
