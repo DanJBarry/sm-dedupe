3c591437f83e38003b0de716c1e3c9c6262d6f9c
Daniel Barry <dbarry@stevens.edu>
2020-08-30 15:17:44 -0400

    Clean up parser

--- src/SmDedupe/Parse.hs
+++ src/SmDedupe/Parse.hs
@@ -11,35 +11,27 @@ import           Data.Char
 import           Text.Parsec
 import           Text.Parsec.ByteString
 
-data Section = Bpms [(Double, Double)] | Chart [[String]] | Extra
+type BpmPair = (Double, Double)
+type MeasureList = [[String]]
 
+data Section = Bpms [BpmPair] | Chart MeasureList | Extra
 data Song = Song
-  { bpms :: [(Double, Double)],
-    charts :: [[[String]]]
+  { bpms :: [BpmPair],
+    charts :: [MeasureList]
   }
   deriving (Show)
 
-endSection :: Parser ()
-endSection = do
-  char ';'
-  manyTill anyChar $ skipMany1 (char '#') <|> eof
-  return ()
+addSection :: Song -> Section -> Song
+addSection song section = case section of
+  Bpms  newBpms  -> song { bpms = newBpms }
+  Chart newChart -> song { charts = newChart : charts song }
+  Extra          -> song
 
-lastMeasure :: Parser [String]
-lastMeasure = do
-  measure <- manyTill row $ char ';'
-  spaces'
-  return measure
-
-row :: Parser String
-row = do
-  result <- manyTill (noneOf "/") spaces'
-  sepEndBy comment spaces'
-  return result
-
-
-rows :: Parser [String]
-rows = sepEndBy1 (many1 alphaNum) endOfLine
+bpm :: Parser BpmPair
+bpm = do
+  beat  <- manyTill anyChar $ char '='
+  value <- many $ noneOf ",;"
+  return (read beat, read value)
 
 comma :: Parser ()
 comma = do
@@ -47,8 +39,37 @@ comma = do
   char ','
   spaces'
 
-measures :: Parser [[String]]
+comment :: Parser ()
+comment = do
+  string "//"
+  manyTill anyChar endOfLine
+  return ()
+
+endSection :: Parser ()
+endSection = do
+  char ';'
+  manyTill anyChar $ skipMany1 (char '#') <|> eof
+  return ()
+
+extra :: Parser Section
+extra = do
+  manyTill anyChar endSection
+  spaces'
+  return Extra
+
+lastMeasure :: Parser [String]
+lastMeasure = do
+  measure <- manyTill row $ char ';'
+  spaces'
+  return measure
+
+measures :: Parser MeasureList
 measures = sepBy1 rows comma
+
+multipleBpms :: Parser [BpmPair]
+multipleBpms = do
+  (result : _) <- manyTill (sepBy1 bpm comma) endSection
+  return result
 
 notes :: Parser Section
 notes = do
@@ -60,39 +81,31 @@ notes = do
   manyTill anyChar $ char ':'
   -- difficulty
   manyTill anyChar $ char ':'
-  -- ???
+  -- radar scores
   manyTill anyChar $ char ':'
   spaces'
   (measures : _) <- manyTill measures endSection
   return $ Chart measures
 
-extra :: Parser Section
-extra = do
-  manyTill anyChar endSection
-  spaces'
-  return Extra
-
-bpm :: Parser (Double, Double)
-bpm = do
-  beat  <- manyTill anyChar $ char '='
-  value <- many $ noneOf ",;"
-  return (read beat, read value)
-
-singleBpm :: Parser [(Double, Double)]
-singleBpm = do
-  beat  <- manyTill anyChar $ char '='
-  value <- manyTill (noneOf ",") endSection
-  return [(read beat, read value)]
-
-multipleBpms :: Parser [(Double, Double)]
-multipleBpms = do
-  (result : _) <- manyTill (sepBy1 bpm comma) endSection
-  return result
-
 parseBpms :: Parser Section
 parseBpms = do
   result <- try singleBpm <|> multipleBpms
   return $ Bpms result
+
+parseSm :: Parser Song
+parseSm = do
+  manyTill anyChar $ char '#'
+  results <- many section
+  return $ foldl addSection Song { bpms = [], charts = [] } results
+
+row :: Parser String
+row = do
+  result <- manyTill (noneOf "/") spaces'
+  sepEndBy comment spaces'
+  return result
+
+rows :: Parser [String]
+rows = sepEndBy1 (many1 alphaNum) endOfLine
 
 section :: Parser Section
 section = do
@@ -102,24 +115,11 @@ section = do
     "BPMS"  -> parseBpms
     _       -> extra
 
-comment :: Parser ()
-comment = do
-  string "//"
-  manyTill anyChar endOfLine
-  return ()
-
-parseSm :: Parser Song
-parseSm = do
-  manyTill anyChar $ char '#'
-  results <- many section
-  return $ foldl
-    (\song section -> case section of
-      Bpms  newBpms  -> song { bpms = newBpms }
-      Chart newChart -> song { charts = newChart : charts song }
-      Extra          -> song
-    )
-    Song { bpms = [], charts = [] }
-    results
+singleBpm :: Parser [BpmPair]
+singleBpm = do
+  beat  <- manyTill anyChar $ char '='
+  value <- manyTill (noneOf ",") endSection
+  return [(read beat, read value)]
 
 -- thank you for putting zero-width spaces in your .sm files
 spaceOrIgnorable :: Parser Char
